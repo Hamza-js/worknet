638506dd628a5c9cacf38320397871a3e5fe7343
Muhammad Hamza <mhamzaawanmp1@gmail.com>
2025-05-08 22:18:35 -0700

    packages page done

--- app/[locale]/(home)/packages/page.tsx
+++ app/[locale]/(home)/packages/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { ChevronRight } from "lucide-react";
+import { Check, ChevronRight } from "lucide-react";
 import { useTranslations } from "next-intl";
 import React, { useState } from "react";
 import Speaker1 from "./../../../../public/assets/icons/speaker1.svg";
@@ -87,21 +87,25 @@ const Packages = () => {
   ];
 
   return (
-    <section className="py-22 bg-gray-50 text-center ">
-      <div className=" mb-40 container mx-auto px-4 w-full md:px-20">
+    <section className="py-10 md:py-22 md:px-12 w-full bg-gray-50 text-center">
+      <div className=" mx-auto px-4 max-w-[1750px]">
         <h2 className="text-2xl md:text-3xl font-bold text-[#1C1B1B] mb-3">
           {t("title")}
         </h2>
-        <p className="text-[#1C1B1B] mb-3 text-sm">{t("description")}</p>
+        <p className="text-[#1C1B1B] mb-3 font-montserrat md:text-base md:leading-4 tracking-normal font-montserrat font-light text-xs leading-5 text-center">
+          {t("description")}
+        </p>{" "}
         <div className="flex w-full items-center text-center justify-center gap-3  p-0">
           <p className="text-[#999BA7]  text-sm">{t("HomePage")}</p>
-          <ChevronRight size={18} color="#999BA7" />
-          <p className="text-[#23BA97]  font-bold text-sm">{t("packages")}</p>
+          <ChevronRight size={24} color="#999BA7" />
+          <p className="text-[#23BA97]  font-montserrat font-semibold text-base leading-none tracking-normal capitalize">
+            {t("packages")}
+          </p>
         </div>
         <div className="flex justify-center items-center gap-4 mt-8">
           <label
             onClick={() => setIsYearly(false)}
-            className={`text-sm font-semibold cursor-pointer ${
+            className={`font-[Montserrat\ arm] font-medium text-base leading-4 tracking-normal text-center cursor-pointer ${
               !isYearly ? "text-[#23BA97]" : "text-[#1C1B1B]"
             }`}
           >
@@ -110,19 +114,18 @@ const Packages = () => {
 
           <label
             onClick={() => setIsYearly(true)}
-            className={`text-sm font-semibold cursor-pointer ${
+            className={`font-[Montserrat\ arm] font-medium text-base leading-4 tracking-normal text-center cursor-pointer ${
               isYearly ? "text-[#23BA97]" : "text-[#1C1B1B]"
             }`}
           >
             Yearly Packages
           </label>
         </div>
-
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-8">
           {plans.map((plan) => (
             <div
               key={plan.id}
-              className={`w-full p-6 bg-white rounded-lg transition-all flex flex-col h-full ${
+              className={`w-full p-5 max-w-[425px] h-[756px] rounded-[30px] bg-white transition-all flex flex-col ${
                 plan.id === "standard"
                   ? "border-2 border-[#23BA97]"
                   : "border-2 border-[#E9EDF5]"
@@ -133,23 +136,33 @@ const Packages = () => {
                   <Image
                     src={plan.icon}
                     alt={`${plan.name} icon`}
-                    width={90}
-                    height={90}
+                    width={142}
+                    height={144}
                   />
                 </div>
                 <div className="text-left space-y-1">
-                  <p className="font-[Montserrat arm] font-normal text-md leading-4 tracking-normal text-[#B8BDD8]">
+                  <p className="font-[Montserrat\ arm] font-normal text-base leading-4 tracking-normal text-[#B8BDD8]">
                     {plan.name}
                   </p>
-                  <p className="font-[Montserrat arm] font-medium text-[22px] leading-[36px] tracking-normal text-[#293B44]">
+                  <p className="font-[Montserrat\ arm] font-medium text-3xl leading-9 tracking-normal text-[#293B44]">
                     {plan.price}
                   </p>
                 </div>
 
-                <ul className="list-none mt-4 text-left text-sm text-[#999BA7]">
+                <ul className="list-none mt-4 text-left space-y-3 ">
                   {plan.features.map((feature, index) => (
-                    <li key={index} className="flex items-center gap-2">
-                      <span className="text-[#23BA97]">✓</span> {feature}
+                    <li
+                      key={index}
+                      className="flex items-center font-[Montserrat\ arm] font-normal text-[15px] leading-4 tracking-normal gap-2 text-[#999BA7]"
+                    >
+                      <span className="text-[#23BA97]">
+                        <Check
+                          size={24}
+                          // fill="bg-[linear-gradient(90deg,_#9796F0_0%,_#FBC7D4_100%)]"
+                          className=""
+                        />
+                      </span>{" "}
+                      {feature}
                     </li>
                   ))}
                 </ul>
@@ -160,7 +173,7 @@ const Packages = () => {
                   onClick={() => setSelectedPlan(plan.id)}
                   variant={plan.id === "standard" ? "primary" : "secondary"}
                   size="small"
-                  className={`w-full py-3 rounded-full text-white font-semibold  ${
+                  className={`w-full h-[48px] py-3 rounded-[24px] text-white font-semibold  ${
                     plan.id !== "standard" && "bg-[#293B44] hover:bg-[#1f2e35]"
                   }`}
                 >
