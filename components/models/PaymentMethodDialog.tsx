45f7d054822bd8f80467cd8de96e7f1fff2f9f6c
Muhammad Hamza <mhamzaawanmp1@gmail.com>
2025-05-22 11:23:02 -0700

    s

--- components/models/PaymentMethodDialog.tsx
+++ components/models/PaymentMethodDialog.tsx
@@ -207,7 +207,7 @@ export default function PaymentMethodDialog({
           </Dialog.DialogDescription>
         </Dialog.DialogHeader>
 
-        <div className="grid grid-cols-2 sm:grid-cols-4 gap-2 mt-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-2 mt-4">
           {paymentMethods.map((method) => (
             <button
               key={method.id}
